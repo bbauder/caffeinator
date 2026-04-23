cc79ffe4023c01db776f91a5a442ec06bc54f0fc
Bruce Bauder <bruce@unknown4e8bbcbc6561.home>
2026-04-23 15:18:12 -0700

    Initial Xcode project for Caffeinator

---
+++ Caffeinator/Caffeinator/CaffeinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaffeinatorApp.swift
+//  Caffeinator
+//
+//  Created by Bruce Bauder on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct CaffeinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
