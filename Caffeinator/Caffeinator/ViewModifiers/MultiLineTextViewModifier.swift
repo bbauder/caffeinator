a1496164fc4dc90bbce72a182f4f9788c1d505ab
Bruce Bauder <bruce.bauder@gmail.com>
2026-05-05 07:59:06 -0700

    Added some comments

--- Caffeinator/Caffeinator/ViewModifiers/MultiLineTextViewModifier.swift
+++ Caffeinator/Caffeinator/ViewModifiers/MultiLineTextViewModifier.swift
@@ -13,6 +13,14 @@ private struct MultiLineTextViewModifier: ViewModifier {
     let multiLineTextAlignment: TextAlignment
     let lineSpacing: CGFloat
 
+    // .fixedSize() determines whether the view accepts or rejects the parent’s size proposal,
+    // not about whether the view is fixed or flexible in either dimension.
+    // In other words, my intent is:
+    //     - Horizontal: Constrain me (wrap text)
+    //     - Vertical: Let me grow (get taller as text wraps)
+    // But .fixedSize() expresses:
+    //     - Horizontal: Don't escape the parent
+    //     - Vertical: Escape the parent
     func body(content: Content) -> some View {
         content
             .fixedSize(horizontal: false, vertical: true)
